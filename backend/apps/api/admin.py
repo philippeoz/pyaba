f3255417d2ee9298d3711d338f738c810d9dfcfd
Philippe Gonzaga <francisco.philippe@gmail.com>
2025-06-13 13:09:44 -0300

    feat: register Registration model with custom admin interface for improved display and filtering

--- backend/apps/api/admin.py
+++ backend/apps/api/admin.py
@@ -2,8 +2,15 @@ from django.contrib import admin
 from apps.api import models
 
 
+class RegistrationAdmin(admin.ModelAdmin):
+    list_display = ("attendee__full_name", "attendee__email", "tutorial__title", "tutorial__event__title")
+    search_fields = ("attendee__name", "tutorial__title")
+    list_filter = ("tutorial__title", "tutorial__event__title")
+    ordering = ("-tutorial__start_datetime", "tutorial__title", "attendee__full_name")
+
+
 admin.site.register(models.Event)
 admin.site.register(models.Tutorial)
 admin.site.register(models.Attendee)
-admin.site.register(models.Registration)
 admin.site.register(models.Instructor)
+admin.site.register(models.Registration, RegistrationAdmin)
